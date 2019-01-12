bdf2fdc3bd9bd36d7a1c2b3e1cee37a22d392274
Harrisonkamau <kamauharrison87@gmail.com>
2019-01-12 11:40:30 +0300

    - Add nationality through migration - Change type of year_of_birth to Int

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -3,6 +3,6 @@ class Types::AuthorType < Types::BaseObject
 
   field :first_name, String, null: false, camelize: false
   field :last_name, String, null: false, camelize: false
-  field :year_of_birth, String, null: true, camelize: false
-
+  field :year_of_birth, Int, null: true, camelize: false
+  field :nationality, String, null: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_081927) do
+ActiveRecord::Schema.define(version: 2019_01_12_083353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_01_12_081927) do
     t.integer "year_of_birth"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nationality"
   end
 
 end
