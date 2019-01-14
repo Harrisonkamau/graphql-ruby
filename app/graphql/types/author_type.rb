eaa35522ee3353f24917336c50c640626471fd13
Harrisonkamau <kamauharrison87@gmail.com>
2019-01-14 11:46:02 +0300

    Return full_name

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -8,9 +8,15 @@ module Types
 
     field :id, ID, null: false
     field :first_name, String, null: false, camelize: false
-    field :last_name, String, null: false, camelize: false
+    field :last_name, String, null: true, camelize: false
     field :year_of_birth, Int, null: true, camelize: false
     field :nationality, String, null: true
+    field :full_name, String, null: true, camelize: false
+
+
+    def full_name
+      ([object.first_name, object.last_name].compact).join(' ')
+    end
   end
 end
 
